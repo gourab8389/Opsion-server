6440732d76eedf45a2239ad1526e387b2516028f
gourab8389 <deyg6988@gmail.com>
2025-08-16 21:27:15 +0530

    feat: implement ticket creation functionality with AI processing

---
+++ controllers/ticket.js
@@ -0,0 +1,35 @@
+import { inngest } from "../inngest/client";
+import Ticket from "../models/ticket";
+
+export const createTicket = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    if (!title || !description) {
+      return res
+        .status(400)
+        .json({ message: "Title and description are required" });
+    }
+    const newTicket = await Ticket.create({
+      title,
+      description,
+      createdBy: req.user._id.toString(),
+    });
+
+    await inngest.send({
+      name: "on-ticket-created",
+      data: {
+        ticketId: newTicket._id.toString(),
+        title,
+        description,
+        createdBy: req.user._id.toString(),
+      },
+    });
+    return res.status(201).json({
+      message: "Ticket created and processing started",
+      ticket: newTicket,
+    });
+  } catch (error) {
+    console.error("Error creating ticket:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
