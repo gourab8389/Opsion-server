0a47203e6c6ad2ff9e74533efc95b23328c438d2
gourab8389 <deyg6988@gmail.com>
2025-08-16 19:43:18 +0530

    feat: enhance user login, logout, and update functionalities with improved error handling and response messages

--- controllers/user.js
+++ controllers/user.js
@@ -42,34 +42,34 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-     const user = await User.findOne({email});
-     if(!user){
-       return res.status(404).json({
-         message: "User not found"
-       });
-     }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
 
-     const isMatch = await bcrypt.compare(password, user.password);
-     if(!isMatch){
-       return res.status(401).json({
-         message: "Invalid credentials"
-       });
-     }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
 
-     const token = jwt.sign(
-       {
-         _id: user._id,
-         role: user.role,
-       },
-       process.env.JWT_SECRET,
-       { expiresIn: "5d" }
-     );
+    const token = jwt.sign(
+      {
+        _id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "5d" }
+    );
 
-     res.status(200).json({
-       message: "Login successful",
-       user,
-       token,
-     });
+    res.status(200).json({
+      message: "Login successful",
+      user,
+      token,
+    });
   } catch (error) {
     res.status(500).json({
       message: "Login failed",
@@ -81,25 +81,78 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    if(!token){
-        return res.status(401).json({
-          message: "Unauthorized"
-        });
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
     }
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
         return res.status(401).json({
-          message: "Invalid token"
+          message: "Invalid token",
         });
       }
       // Token is valid, proceed with logout
       res.status(200).json({
-        message: "Logout successful"
+        message: "Logout successful",
       });
     });
   } catch (error) {
     res.status(500).json({
       message: "Logout failed",
+      error: error.message,
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { skills = [], role, email } = req.body;
+
+  try {
+    if (req.user?.role !== "admin") {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    await User.updateOne(
+      { email },
+      { skills: skills.length ? skills : user.skills, role }
+    );
+
+    res.status(200).json({
+      message: "User updated successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "User update failed",
+      error: error.message,
+    });
+  }
+};
+
+export const getUser = async (req, res) => {
+  try {
+    if (req.user.role !== "admin") {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+
+    const users = await User.find().select("-password");
+    res.status(200).json({
+      message: "Users fetched successfully",
+      users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to fetch users",
       error: error.message,
     });
   }
